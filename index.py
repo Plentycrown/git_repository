3811b026821be63cb9f9232e16e74a773387aa91
Plentycrown <plentycrown@gmail.com>
2023-04-05 10:58:39 +0100

    modify index and print another message

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
- print("hello from the index file")
+print("hello from the index file")
+
+
+def average_mean(x):
+    count = 0 
+    total = 0
+    for i in x:
+       count = count + 1
+       avg = total + i
+    return (avg/count))
+
+y = (1,2,3,4,5)
+print(average_mean(y))
